ce1bc7cc03c6901e161147a7d5dd572c29215216
charlie robson <charlie.robson@b2m-solutions.com>
2020-01-13 10:59:55 +0000

    refactor for readability

--- SNAKBIRD/snakbird.c
+++ SNAKBIRD/snakbird.c
@@ -9,8 +9,8 @@
 typedef unsigned char byte;
 typedef void(*UDFN)(void);
 
-char *display;
-extern int d_file @16396;
+char *_display;
+extern int D_FILE @16396;
 
 #define K_UP       'Q'
 #define K_DOWN     'A'
@@ -27,11 +27,19 @@ enum
 	RESET
 };
 
-byte snake[16] = {0};
-byte snakeHead, snakeTail, snakeFace, snakeLen, snakeDead;
-byte fruit, doorOpen;
 
-int level;
+typedef struct {
+	byte segs[16];
+	byte head, tail, face, length, isDead;
+} snake_t;
+
+snake_t _snake;
+
+
+int _level;
+int _levelCount;
+byte _fruitCount, _doorIsOpen;
+
 
 enum {
 	T_EMPTY,
@@ -60,16 +68,19 @@ enum {
 	T_LAST
 };
 
-int tileMap[] = {
+
+int _tileMap[] = {
+	// unsolids
 	0,0,0,0,				// air
-
 	0x3d,0x3d,0x3d,0x3d,	// spikes
-	0x8a,0x8a,0x80,0x80,	// ground
 
-	0x81,0x82,0x84,0x07,	// 'fruit'
+	// solids
+	0x8a,0x8a,0x80,0x80,	// ground
+	0x81,0x82,0x84,0x07,	// '_fruitCount'
 	0x87,0x04,0x02,0x01,	// door, closed
 	0x07,0x84,0x82,0x81,	// door, open
 
+	// snakes
 	0x08,0x08,0x08,0x08,	// snakebod
 	0x80,0x80,0x80,0x80,
 
@@ -87,6 +98,7 @@ int tileMap[] = {
 	0x1b,0x1b,0x1b,0x1b,	// bod dead 2
 };
 
+
 #define A 0x80
 #define B 0x81
 #define C 0x82
@@ -100,8 +112,8 @@ int tileMap[] = {
 
 const int MAPSIZE = 16*12;
 
-byte map[16*12];
-byte gameMap[] = {
+byte _map[16*12];
+byte _gameMap[] = {
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,F,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -134,13 +146,14 @@ byte gameMap[] = {
 	0,0,0,0,0,0,0,0,X,0,0,0,0,0,0,0,
 	0,0,M,M,M,M,0,0,0,0,0,0,0,0,0,0,
 	0,0,M,0,F,M,0,0,0,0,0,0,0,0,0,0,
-	0,0,M,0,0,M,0,0,0,C,0,0,0,0,0,0,
-	0,0,M,0,0,0,0,0,A,B,0,F,0,0,0,0,
+	0,0,M,0,0,M,0,0,C,0,0,0,0,0,0,0,
+	0,0,M,0,0,0,0,0,B,A,0,F,0,0,0,0,
 	0,0,M,M,M,M,M,M,M,M,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 
+	0xf0,0xff
 };
 
 
@@ -148,105 +161,106 @@ byte gameMap[] = {
 void putBlock(int cellId, int tile) {
 	int cellX = cellId & 15;
 	int cellY = cellId / 16;
-	char* cell = display + (cellX * 2) + (cellY * 66);
+	char* cell = _display + (cellX * 2) + (cellY * 66);
 	int ci = tile * 4;
 
-	cell[0] = tileMap[ci];
+	cell[0] = _tileMap[ci];
 	++ci;
-	cell[1] = tileMap[ci];
+	cell[1] = _tileMap[ci];
 	++ci;
-	cell[33] = tileMap[ci];
+	cell[33] = _tileMap[ci];
 	++ci;
-	cell[34] = tileMap[ci];
+	cell[34] = _tileMap[ci];
 }
 
 
 void renderMap() {
 	for (int i = 0; i < MAPSIZE; ++i) {
-		putBlock(i, map[i]);
+		putBlock(i, _map[i]);
 	}
 }
 
 
-// update map and track which cells have changed for drawing
-byte updateList[32];
-byte updateListCount;
+// update _map and track which cells have changed for drawing
+byte _updateList[32];
+byte _updateListCount;
 void updateMap(int index, byte value) {
-	map[index] = value;
+	_map[index] = value;
 
 	// only render a cell once
-	for (int i = 0; i < updateListCount; ++i) {
-		if (updateList[i] == index)
+	for (int i = 0; i < _updateListCount; ++i) {
+		if (_updateList[i] == index)
 			return;
 	}
 
-	updateList[updateListCount] = index;
-	++updateListCount;
+	_updateList[_updateListCount] = index;
+	++_updateListCount;
 }
 
 
 void renderUpdates() {
-	for (int i = 0; i < updateListCount; ++i) {
-		byte cell = updateList[i];
-		putBlock(cell, map[cell]);
+	for (int i = 0; i < _updateListCount; ++i) {
+		byte cell = _updateList[i];
+		putBlock(cell, _map[cell]);
 	}
-	updateListCount = 0;
+	_updateListCount = 0;
 }
 
 
-int udTimer;
-UDFN updateFn;
+// game update function handler
+int _udTimer;
+UDFN _updateFn;
 int doUpdateFn() {
-	if (updateFn) {
-		updateFn();
+	if (_updateFn) {
+		_updateFn();
 	}
-	return updateFn != NULL;
+	return _updateFn != NULL;
 }
 
 
 void putSnakeInMap() {
-	if (!snakeDead) {
-		updateMap(snake[snakeHead], snakeFace + T_FACEUP);
-		for (int i = 1, n = snakeHead; i < snakeLen; ++i) {
+	if (!_snake.isDead) {
+		updateMap(_snake.segs[_snake.head], _snake.face + T_FACEUP);
+		for (int i = 1, n = _snake.head; i < _snake.length; ++i) {
 			n = (n - 1) & 15;
-			updateMap(snake[n], T_SNAKEBOD2 - (i & 1));
+			updateMap(_snake.segs[n], T_SNAKEBOD2 - (i & 1));
 		}
 	} else {
-		int phase = udTimer / 10;
+		int phase = _udTimer / 10;
 		switch(phase) {
 			case 0:
 			case 1:
 			case 2:
 			case 3:
 			{
-				int x = snakeDead ? T_DEADFACEUP : T_FACEUP;
-				updateMap(snake[snakeHead], snakeFace + x);
-				for (int i = 1, n = snakeHead; i < snakeLen; ++i) {
+				int x = _snake.isDead ? T_DEADFACEUP : T_FACEUP;
+				updateMap(_snake.segs[_snake.head], _snake.face + x);
+				for (int i = 1, n = _snake.head; i < _snake.length; ++i) {
 					n = (n - 1) & 15;
-					updateMap(snake[n], T_SNAKEBOD2 - (i & 1));
+					updateMap(_snake.segs[n], T_SNAKEBOD2 - (i & 1));
 				}
 			}
 			break;
 			case 4:
 			{
-				for (int i = 0, n = snakeHead; i < snakeLen; ++i) {
-					updateMap(snake[n], T_BODDEAD1);
+				for (int i = 0, n = _snake.head; i < _snake.length; ++i) {
+					updateMap(_snake.segs[n], T_BODDEAD1);
 					n = (n - 1) & 15;
 				}
 			}
 			break;
 			case 5:
 			{
-				for (int i = 0, n = snakeHead; i < snakeLen; ++i) {
-					updateMap(snake[n], T_BODDEAD2);
+				for (int i = 0, n = _snake.head; i < _snake.length; ++i) {
+					updateMap(_snake.segs[n], T_BODDEAD2);
 					n = (n - 1) & 15;
 				}
 			}
 			break;
 			default:
 			{
-				for (int i = 0, n = snakeHead; i < snakeLen; ++i) {
-					updateMap(snake[n], T_EMPTY);
+				for (int i = 0, n = _snake.head; i < _snake.length; ++i) {
+					updateMap(_snake.segs[n], T_EMPTY);
 					n = (n - 1) & 15;
 				}
 			}
@@ -258,27 +272,27 @@ void putSnakeInMap() {
 
 
 void setUpdateFn(UDFN fn) {
-	udTimer = 0;
-	updateFn = fn;
+	_udTimer = 0;
+	_updateFn = fn;
 }
 
 
 void nextLevel(void) {
-	++udTimer;
-	if (udTimer == 100) {
-		++level;
-		level %= 3;
-		reset();
+	++_udTimer;
+	if (_udTimer == 100) {
+		++_level;
+		_level %= _levelCount;
+		reset(_level);
 	}
 }
 
 
 void death(void) {
-	snakeDead = 1;
+	_snake.isDead = 1;
 
-	++udTimer;
-	if (udTimer == 100) {
-		reset();
+	++_udTimer;
+	if (_udTimer == 100) {
+		reset(_level);
 	}
 }
 
@@ -287,17 +301,17 @@ void exiting(void) {
 	int exited = 1;
 
 	// collapse all segments onto the head, counting how many segments are in one place
-	updateMap(snake[snakeTail], T_EMPTY);
-	for (int i = 0; i < snakeLen - 1; ++i) {
-		if (snake[(snakeTail + i) & 15] == snake[snakeHead]) {
+	updateMap(_snake.segs[_snake.tail], T_EMPTY);
+	for (int i = 0; i < _snake.length - 1; ++i) {
+		if (_snake.segs[(_snake.tail + i) & 15] == _snake.segs[_snake.head]) {
 			++exited;
 		}
-		snake[(snakeTail + i) & 15] = snake[(snakeTail + 1 + i) & 15];
+		_snake.segs[(_snake.tail + i) & 15] = _snake.segs[(_snake.tail + 1 + i) & 15];
 	}
 
-	if (exited == snakeLen) {
+	if (exited == _snake.length) {
 		setUpdateFn(nextLevel);
-		updateMap(snake[snakeHead], T_DOORCLOSED);
+		updateMap(_snake.segs[_snake.head], T_DOORCLOSED);
 	}
 
 	return 1;
@@ -305,19 +319,19 @@ void exiting(void) {
 
 
 void countFruit() {
-	fruit = 0;
+	_fruitCount = 0;
 	for (int i = 0; i < MAPSIZE; ++i) {
-		if (map[i] == T_FRUIT1) {
-			++fruit;
+		if (_map[i] == T_FRUIT1) {
+			++_fruitCount;
 		}
 	}
 }
 
 
 void openDoor() {
-	doorOpen = 1;
+	_doorIsOpen = 1;
 	for (int i = 0; i < MAPSIZE; ++i) {
-		if (map[i] == T_DOORCLOSED) {
+		if (_map[i] == T_DOORCLOSED) {
 			updateMap(i, T_DOOROPEN);
 			return;
 		}
@@ -337,15 +351,15 @@ int isTraversible(byte blockType) {
 
 
 int checkFall() {
-	for (int i = 0, n = snakeTail; i < snakeLen; ++i) {
+	for (int i = 0, n = _snake.tail; i < _snake.length; ++i) {
 		// if any segment would go off screen then die
-		if (snake[n] + 16 >= MAPSIZE) {
+		if (_snake.segs[n] + 16 >= MAPSIZE) {
 			setUpdateFn(death);
 			return 1;
 		} 
 
-		// if any segment is supported by something other than air, spikes or another segment then nothing to do
-		if (map[snake[n] + 16] > T_SPIKES && map[snake[n] + 16] < T_SNAKEBOD1)
+		// if any segment is supported by something solid other than another segment then nothing to do
+		if (_map[_snake.segs[n] + 16] > T_SPIKES && _map[_snake.segs[n] + 16] < T_SNAKEBOD1)
 			return 0;
 
 		n = (n + 1) & 15;
@@ -353,9 +367,9 @@ int checkFall() {
 
 	// we would be falling so check for hazards
 
-	for (int i = 0, n = snakeTail; i < snakeLen; ++i) {
+	for (int i = 0, n = _snake.tail; i < _snake.length; ++i) {
 		// if any segment would go into spikes then die
-		if (map[snake[n] + 16] == T_SPIKES) {
+		if (_map[_snake.segs[n] + 16] == T_SPIKES) {
 			setUpdateFn(death);
 			return 1;
 		} 
@@ -365,9 +379,9 @@ int checkFall() {
 
 
 	// none of the segments are supported so move them all down
-	for (int i = 0, n = snakeTail; i < snakeLen; ++i) {
-		updateMap(snake[n], T_EMPTY);
-		snake[n] = snake[n] + 16;
+	for (int i = 0, n = _snake.tail; i < _snake.length; ++i) {
+		updateMap(_snake.segs[n], T_EMPTY);
+		_snake.segs[n] = _snake.segs[n] + 16;
 		n = (n + 1) & 15;
 	}
 
@@ -375,32 +389,33 @@ int checkFall() {
 }
 
 
-int dirs[] = {
-	-16,1,16,-1
-};
 int tryMove(int newDirn) {
-	int newMapPos = snake[snakeHead] + dirs[newDirn];
-	if (!isTraversible(map[newMapPos]))
+	const int _dirs[] = {
+		-16,1,16,-1
+	};
+
+	int newMapPos = _snake.segs[_snake.head] + _dirs[newDirn];
+	if (!isTraversible(_map[newMapPos]))
 		return 0;
 
-	snakeFace = newDirn;
-	snakeHead = (snakeHead + 1) & 15;
-	snake[snakeHead] = newMapPos;
+	_snake.face = newDirn;
+	_snake.head = (_snake.head + 1) & 15;
+	_snake.segs[_snake.head] = newMapPos;
 
-	if (map[newMapPos] != T_FRUIT1) {
-		// update tail if not moving on to fruit
-		updateMap(snake[snakeTail], T_EMPTY);
-		snakeTail = (snakeTail + 1) & 15;
+	if (_map[newMapPos] != T_FRUIT1) {
+		// update tail if not moving on to _fruitCount
+		updateMap(_snake.segs[_snake.tail], T_EMPTY);
+		_snake.tail = (_snake.tail + 1) & 15;
 	}
 
-	if (map[newMapPos] == T_FRUIT1) {
-		// moved on to fruit, so grow
-		++snakeLen;
-		--fruit;
-	} else if (map[newMapPos] == T_DOOROPEN)
+	if (_map[newMapPos] == T_FRUIT1) {
+		// moved on to _fruitCount, so grow
+		++_snake.length;
+		--_fruitCount;
+	} else if (_map[newMapPos] == T_DOOROPEN)
 		setUpdateFn(exiting);
 
-	if (!doorOpen && !fruit) {
+	if (!_doorIsOpen && !_fruitCount) {
 		openDoor();
 	}
 
@@ -426,35 +441,38 @@ void snakeMove() {
 		if (k == K_DOWN) return tryMove(DOWN);
 		if (k == K_LEFT) return tryMove(LEFT);
 		if (k == K_RIGHT) return tryMove(RIGHT);
-		if (k == K_RESET) return reset();
+		if (k == K_RESET) return reset(_level);
 	}
 }
 
 
-int reset() {
-	snakeDead = 0;
-	doorOpen = 0;
+int reset(int level) {
+	_level = level;
+	_doorIsOpen = 0;
+
+	_updateListCount = 0;
+
+	_snake.isDead = 0;
 
 	setUpdateFn(snakeMove);
-	updateListCount = 0;
 
-	int levelOffset = level * MAPSIZE;
-	memcpy(map, gameMap + levelOffset, MAPSIZE);
+	int levelOffset = _level * MAPSIZE;
+	memcpy(_map, _gameMap + levelOffset, MAPSIZE);
 
-	snakeLen = 0;
+	_snake.length = 0;
 	for (int i = 0; i < MAPSIZE; ++i) {
-		if (map[i] > 0x7f) {
-			snake[(0x80 - map[i]) & 15] = i;
-			++snakeLen;
+		if (_map[i] > 0x7f) {
+			_snake.segs[(0x80 - _map[i]) & 15] = i;
+			++_snake.length;
 		}
 	}
-	snakeHead = 0;
-	snakeTail = 17 - snakeLen;
-	snakeFace = snake[0] - snake[15];
-	if (snakeFace == -1) snakeFace = LEFT;
-	else if (snakeFace == 1) snakeFace = RIGHT;
-	else if (snakeFace == -16) snakeFace = UP;
-	else snakeFace = DOWN;
+	_snake.head = 0;
+	_snake.tail = 17 - _snake.length;
+	_snake.face = _snake.segs[0] - _snake.segs[15];
+	if (_snake.face == -1) _snake.face = LEFT;
+	else if (_snake.face == 1) _snake.face = RIGHT;
+	else if (_snake.face == -16) _snake.face = UP;
+	else _snake.face = DOWN;
 
 	countFruit();
 
@@ -465,11 +483,12 @@ int reset() {
 
 void main()
 {
-	display = d_file + 1;
+	_display = D_FILE + 1;
 
-	level = 0;
+	for (_levelCount = 0; _gameMap[_levelCount * MAPSIZE] != 0xf0 && _gameMap[_levelCount * MAPSIZE + 1] != 0xff; ++_levelCount) {}
 
-	reset();
+	reset(0);
+
 	while(1) {
 		putSnakeInMap();
 		renderUpdates();
